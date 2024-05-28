ea6ad6751b6c556588fbc6ae5be7f098896aa7d5
Leon <leonzapke@gmail.com>
2024-05-28 21:57:02 +0200

    - Updated .gitignore

--- app/src/main/java/de/ljz/questify/MainActivity.kt
+++ app/src/main/java/de/ljz/questify/MainActivity.kt
@@ -15,11 +15,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import de.ljz.questify.ui.theme.QuestifyTheme
+import io.sentry.Sentry
 
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     enableEdgeToEdge()
+
     setContent {
       QuestifyTheme {
         Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
@@ -32,7 +34,6 @@ class MainActivity : ComponentActivity() {
             Button(onClick = {  }) {
               Text(text = "Hallo")
             }
-            LinearProgressIndicator(progress = 7.0f)
           }
         }
       }
