3b214eecd8dd602127d76d7527e8a0ebb463a825
Leon Zapke <lnzpk.dev@gmail.com>
2024-06-10 14:30:04 +0200

    - Improved "extensions"

--- app/src/main/kotlin/de/ljz/questify/util/Extensions.kt
+++ app/src/main/kotlin/de/ljz/questify/util/Extensions.kt
@@ -1,6 +1,5 @@
 package de.ljz.questify.util
 
-import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.awaitFirstDown
@@ -12,12 +11,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
-import androidx.compose.ui.draw.drawWithContent
-import androidx.compose.ui.graphics.ClipOp
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.drawscope.clipRect
 import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.graphics.nativeCanvas
 import androidx.compose.ui.input.pointer.pointerInput
 
 
@@ -25,7 +19,10 @@ enum class ButtonState { Pressed, Idle }
 
 fun Modifier.bounceClick() = composed {
   var buttonState by remember { mutableStateOf(ButtonState.Idle) }
-  val scale by animateFloatAsState(if (buttonState == ButtonState.Pressed) 0.70f else 1f)
+  val scale by animateFloatAsState(
+    targetValue = if (buttonState == ButtonState.Pressed) 0.70f else 1f,
+    label = "bounceClick"
+  )
 
   this
     .graphicsLayer {
@@ -48,29 +45,4 @@ fun Modifier.bounceClick() = composed {
         }
       }
     }
-}
-
-fun Modifier.typewriterAnimation(): Modifier = composed {
-  val animatedProgress = remember { Animatable(0f) }
-
-  this.then(
-    Modifier.drawWithContent {
-
-      drawContent()
-
-      val textWidth = drawContext.size.toDpSize().width.value * animatedProgress.value
-
-      with(drawContext.canvas.nativeCanvas) {
-        clipRect(
-          left = 0f,
-          top = 0f,
-          right = textWidth,
-          bottom = size.height.toFloat(),
-          clipOp = ClipOp.Intersect
-        ) {
-          drawRect(Color.White)
-        }
-      }
-    }
-  )
 }
