c5ea01c4b72b4bad35443ad32fcc08d964a33090
Leon <leonzapke@gmail.com>
2024-07-17 15:30:28 +0200

    - Gradle upgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
   alias(libs.plugins.jetbrainsCompose) apply false
   alias(libs.plugins.compose.compiler) apply false
 
-  id("com.android.library") version "8.5.0" apply false
+  id("com.android.library") version "8.5.1" apply false
   id("com.google.devtools.ksp") version "2.0.0-1.0.21" apply false
   id("com.google.gms.google-services") version "4.4.2" apply false
   id("com.google.firebase.crashlytics") version "3.0.1" apply false
