83f3087c667597e69f3cbaf15d106ce16721bf9a
Leon <leonzapke@gmail.com>
2024-11-04 14:04:29 +0100

    - bump version to 4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,8 +53,8 @@ android {
         applicationId = "de.ljz.questify"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "0.3"
+        versionCode = 4
+        versionName = "0.4-DEV"
         resourceConfigurations += listOf("en", "de")
     }
 
