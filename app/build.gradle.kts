f58e68bc4a4bf71f3160387bc1beb520d23ea2f9
Leon <leonzapke@gmail.com>
2025-01-01 22:06:32 +0100

    - Version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,8 +53,8 @@ android {
         applicationId = "de.ljz.questify"
         minSdk = 26
         targetSdk = 35
-        versionCode = 6
-        versionName = "0.5"
+        versionCode = 7
+        versionName = "0.5.1"
         resourceConfigurations += listOf("en", "de")
     }
 
