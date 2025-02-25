10d6c5c0f12f944a7aa39eaa21a266a171777526
Leon <leonzapke@gmail.com>
2025-02-25 14:53:14 +0100

    - Removed WearOS module - Updated AGP to 8.8.1 - Updated navigation compose to 2.8.7 - Updated runtime to 1.7.8 - Updated room to 2.7.0-beta01

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
     alias(libs.plugins.jetbrainsCompose) apply false
     alias(libs.plugins.compose.compiler) apply false
 
-    id("com.android.library") version "8.8.0" apply false
+    id("com.android.library") version "8.8.1" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
 //    id("com.google.firebase.crashlytics") version "3.0.2" apply false
-    id("androidx.navigation.safeargs") version "2.8.6" apply false
+    id("androidx.navigation.safeargs") version "2.8.7" apply false
     kotlin("plugin.serialization") version "2.1.0" apply false
     id("com.google.dagger.hilt.android") version "2.55" apply false
     id("com.google.devtools.ksp") version "2.1.0-1.0.29" apply false
-    id("androidx.room") version "2.7.0-alpha13" apply false
+    id("androidx.room") version "2.7.0-beta01" apply false
 }
