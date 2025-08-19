f4d586469a5ce9a699a9a295e7174385590e9047
leonzapke <leonzapke@gmail.com>
2025-08-19 14:32:59 +0200

    - Integrated Google Play Billing library - Added Billing library dependencies to app's build file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -104,6 +104,9 @@ val roomVersion by extra("2.6.1")
 val ktorVersion by extra("2.2.1")
 
 dependencies {
+    implementation(libs.billing)
+    implementation(libs.billing.ktx)
+
     // FONT
     implementation(libs.androidx.ui.text.google.fonts)
 
