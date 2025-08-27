33f9aff5979d4e1db590d26e7deba1a0e9bdd0c1
leonzapke <leonzapke@gmail.com>
2025-08-27 14:06:53 +0200

    - Implemented `AppDragHandle` composable for use in `AppModalBottomSheet` - Refactored `AppModalBottomSheet` to include the new `AppDragHandle`

--- app/src/main/kotlin/de/ljz/questify/core/presentation/components/bottom_sheets/AppModalBottomSheet.kt
+++ app/src/main/kotlin/de/ljz/questify/core/presentation/components/bottom_sheets/AppModalBottomSheet.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.BottomSheetDefaults
@@ -15,6 +16,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.ModalBottomSheetProperties
 import androidx.compose.material3.SheetState
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.contentColorFor
 import androidx.compose.material3.rememberModalBottomSheetState
@@ -22,8 +24,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+
+val DragHandleVerticalPadding = 0.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -56,12 +62,21 @@ fun AppModalBottomSheet(
                     .padding(vertical = 8.dp),
                 contentAlignment = Alignment.Center
             ) {
-                Text(
-                    text = title,
-                    color = MaterialTheme.colorScheme.onSurface,
-                    style = MaterialTheme.typography.titleMedium,
-                    textAlign = TextAlign.Center
-                )
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    AppDragHandle(
+                        modifier = Modifier.padding(vertical = 8.dp)
+
+                    )
+
+                    Text(
+                        text = title,
+                        color = MaterialTheme.colorScheme.onSurface,
+                        style = MaterialTheme.typography.titleMedium,
+                        textAlign = TextAlign.Center
+                    )
+                }
 
                 IconButton(
                     onClick = onDismissRequest,
@@ -76,5 +91,24 @@ fun AppModalBottomSheet(
 
             content()
         }
+    }
+}
+
+@Composable
+fun AppDragHandle(
+    modifier: Modifier = Modifier,
+    width: Dp = 32.0.dp,
+    height: Dp = 4.0.dp,
+    shape: Shape = MaterialTheme.shapes.extraLarge,
+    color: Color = MaterialTheme.colorScheme.onSurfaceVariant,
+) {
+    Surface(
+        modifier =
+            modifier
+                .padding(vertical = DragHandleVerticalPadding),
+        color = color,
+        shape = shape,
+    ) {
+        Box(Modifier.size(width = width, height = height))
     }
 }
