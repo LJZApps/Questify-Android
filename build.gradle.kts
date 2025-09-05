fada89374d3236ba27dd8d2d548f807cc87257d8
leonzapke <leonzapke@gmail.com>
2025-09-05 08:37:36 +0200

    - Updated versions for various dependencies including AGP, Hilt, Kotlin, Lifecycle, Material, Sentry, Compose BOM, and Wear Compose. - Updated plugin versions for Android Library, Hilt, and KSP in the root `build.gradle.kts`.

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
     alias(libs.plugins.jetbrainsCompose) apply false
     alias(libs.plugins.compose.compiler) apply false
 
-    id("com.android.library") version "8.12.1" apply false
+    id("com.android.library") version "8.13.0" apply false
     id("com.google.gms.google-services") version "4.4.3" apply false
 //    id("com.google.firebase.crashlytics") version "3.0.2" apply false
     id("androidx.navigation.safeargs") version "2.9.3" apply false
     kotlin("plugin.serialization") version "2.1.0" apply false
-    id("com.google.dagger.hilt.android") version "2.57" apply false
-    id("com.google.devtools.ksp") version "2.2.0-2.0.2" apply false
+    id("com.google.dagger.hilt.android") version "2.57.1" apply false
+    id("com.google.devtools.ksp") version "2.2.10-2.0.2" apply false
     id("androidx.room") version "2.7.2" apply false
 }
