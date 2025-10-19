1c942576e87457f8c1fd0b0839030cef05911af0
Leon <leonzapke@gmail.com>
2025-10-19 17:54:18 +0200

    Es wurden ein paar Dateien geändert

--- app/src/main/kotlin/de/ljz/questify/core/presentation/screens/ActivityMain.kt
+++ app/src/main/kotlin/de/ljz/questify/core/presentation/screens/ActivityMain.kt
@@ -112,7 +112,6 @@ class ActivityMain : AppCompatActivity() {
                                 navController = navController
                             )
 
-
                             settingRoutes(
                                 navController = navController
                             )
--- app/src/main/kotlin/de/ljz/questify/feature/player_stats/presentation/screens/stats/StatsUiEvent.kt
+++ app/src/main/kotlin/de/ljz/questify/feature/player_stats/presentation/screens/stats/StatsUiEvent.kt
@@ -1,7 +1,7 @@
 package de.ljz.questify.feature.player_stats.presentation.screens.stats
 
-sealed class StatsUiEvent {
-    data object NavigateUp : StatsUiEvent()
+sealed interface StatsUiEvent {
+    data object NavigateUp : StatsUiEvent
 
-    data object ToggleDrawer : StatsUiEvent()
+    data object ToggleDrawer : StatsUiEvent
 }
